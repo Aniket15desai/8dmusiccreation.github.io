46870918f413320aaa39e083baba4b596165b912
Aniket15desai <omkarsardesai14@gmail.com>
2020-09-19 19:31:28 +0530

    HI

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.menu').click(function(){
+        $('ul').toggleClass('active')
+    })
+})
+
+    $(document).ready(function(){
+    $('.search').click(function(){
+        $('ul .M').toggleClass('active1')})
+})
